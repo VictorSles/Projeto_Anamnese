d62cb0b1bfb57ba9e3a40228e50c36ee3e3cb588
Victor_DSales <101744156+VictorSles@users.noreply.github.com>
2023-06-22 00:33:36 -0400

    e

--- scripts/javaScript.js
+++ scripts/javaScript.js
@@ -2,5 +2,18 @@ function saveDate() {
     let glicose = document.getElementById("iglicose").value;
     let pressao = document.getElementById("ipressao").value;
     let peso = document.getElementById("ipeso").value;
-    
+    if ((glicose != "") && (pressao != "") && (peso != "")) {
+        let dates = `
+        <table style="margin-top: 0%; width: 100%">
+            <tr>
+                <td style="max-width: 20px;">${pressao}</td>
+                <td style="max-width: 20px;">${glicose}</td>
+                <td style="max-width: 20px;">${peso}</td>
+            </tr>
+        </table>
+    `
+        document.getElementById("idiv_tree").innerHTML += dates;
+        glicose = ""
+    glicose.focus()
+    }
 }
