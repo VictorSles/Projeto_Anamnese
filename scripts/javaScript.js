d479ba4a526203d5435b8741e8a54f9c587a30cc
Victor_DSales <101744156+VictorSles@users.noreply.github.com>
2023-06-22 01:23:21 -0400

    r

--- scripts/javaScript.js
+++ scripts/javaScript.js
@@ -1,19 +1,23 @@
+let glicose = document.getElementById("iglicose");
+let pressao = document.getElementById("ipressao");
+let peso = document.getElementById("ipeso");
 function saveDate() {
-    let glicose = document.getElementById("iglicose").value;
-    let pressao = document.getElementById("ipressao").value;
-    let peso = document.getElementById("ipeso").value;
-    if ((glicose != "") && (pressao != "") && (peso != "")) {
+    glicose1 = glicose.value;
+    pressao1 = pressao.value;
+    peso1 = peso.value;
+    if ((glicose1 != "") && (pressao1 != "") && (peso1 != "")) {
         let dates = `
         <table style="margin-top: 0%; width: 100%">
             <tr>
-                <td style="max-width: 20px;">${pressao}</td>
-                <td style="max-width: 20px;">${glicose}</td>
-                <td style="max-width: 20px;">${peso}</td>
+                <td style="max-width: 20px;">${pressao1}</td>
+                <td style="max-width: 20px;">${glicose1}</td>
+                <td style="max-width: 20px;">${peso1}</td>
             </tr>
         </table>
     `
         document.getElementById("idiv_tree").innerHTML += dates;
-        glicose = ""
-    glicose.focus()
+        glicose.value = "";
+        pressao.value = "";
+        peso.value = "";
     }
 }
